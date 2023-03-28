ae9f1f5a1f59bf65ebdb71dee83c3e3d54706cbb
jamesuanhoro <jamesuanhoro@gmail.com>
2023-03-28 15:07:07 -0500

    base of new program

---
+++ R/minorbsem-package.R
@@ -0,0 +1,16 @@
+#' The 'minorbsem' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name minorbsem-package
+#' @aliases minorbsem
+#' @useDynLib minorbsem, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.26.21. https://mc-stan.org
+#'
+NULL
